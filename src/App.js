4ef83590cc1c9243a144e5f7529c4689b7120ee3
Sabah <67190764+DBCooper200@users.noreply.github.com>
2022-07-16 17:53:39 +0530

    landing page, login page, signup page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Landing from './container/Landing';
 import Login from './container/login';
 import Signup from './container/signup';
 function App() {
   return (
     <div className="App">
+      <Landing/>
       <Login /> 
       <Signup/>
     </div>
