733bc994f2a1fd3af8703f24b953c8566fc21213
SoumyaGurram <gurramsoumyadevender@gmail.com>
2022-07-19 18:57:41 +0530

    adding landingpage login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import ReactDOM from "react-dom/client";
 import React from 'react';
-import './App.css';
-import Login from './components/login';
+//import './App.css';
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Landing from './pages/landing/landing';
+import Login from './pages/login/login';
+import Signup from "./pages/signup/signup";
 function App() {
   return (
-    <div className="App">
-      
-     <Login/>
-    </div>
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+      </Routes>
+      </BrowserRouter> 
+     </>
   );
 }
 
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {Grid,Paper,Avatar,TextField,Button,Typography,Link} from '@mui/material';
+import {Grid,Paper,Avatar,TextField,Button,Typography } from '@mui/material';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormcontrolLabel from '@material-ui/core/FormcontrolLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+
+import {  Link } from 'react-router-dom';
 const Login=()=>{
      const paperStyle={padding :20,height:'70vh',width:280,margin:"20px auto"}
      const avatarStyle={backgroundColor:'#1bbd7e'}
      const btnstyle={margin:'8px 0'}
     return(
+        <div>
         <Grid>
             <Paper elevation={10} style={paperStyle}> 
             <Grid align='center'>
@@ -28,17 +31,23 @@ const Login=()=>{
             />
             <Button type='submit' color='primary'variant="contained" style={btnstyle}fullwidth>Sign in</Button>
             <Typography>
-                <Link href="#" >
+                <a href="#" >
                  Forgot password?
-                </Link>
+                </a>
                 </Typography>
-                <Typography>create your account
-                <Link href="#" >
+                <Typography>Create your account
+                <Link to="/signup" >
                  Sign up
                 </Link>
                 </Typography>
             </Paper>
         </Grid>
+        
+        
+            
+        
+        
+        </div>
     );
 }
 export default Login;
